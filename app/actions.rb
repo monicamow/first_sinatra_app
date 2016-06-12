4068efdbb45324061967a289f70811623f9b192b
monicamow <mow.monica@gmail.com>
2016-06-11 20:19:15 -0700

    added single message display; before validations

--- app/actions.rb
+++ app/actions.rb
@@ -8,6 +8,26 @@ get '/messages' do
   erb :'messages/index'
 end
 
+# get '/messages/' do 
+#   @messages = Message.all
+#   erb :'messages/index'
+# end
+
 get '/messages/new' do 
   erb :'messages/new'
+end
+
+post '/messages' do 
+  @message = Message.new(
+    title: params[:title],
+    content: params[:content],
+    author: params[:author]
+    )
+  @message.save
+  redirect '/messages'
+end
+
+get '/messages/:id' do 
+  @message = Message.find(params[:id])
+  erb :'messages/show'
 end
